4b25b0e500292fa34a5bf0957ecf4a2f8367546a
Neo Siwela <neosiwela18@gmail.com>
2026-08-31 13:00:06 +0200

    Update file path for customers.csv

--- cleaning.py
+++ cleaning.py
@@ -2,7 +2,7 @@ import csv
 vips = []
 newbies = []
 regualrs = []
-with open("C:\\Users\\acer\\Desktop\\Coding\\Python\\data projects\\ages\\customers.csv", newline="", encoding="utf-8") as user:
+with open("customers.csv", newline="", encoding="utf-8") as user:
 
     man = csv.DictReader(user)
 
